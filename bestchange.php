00e97e77816320159887ca42e8dfb9d8b9f14437
Montasir Rahman Utsho <47495795+imontasirrahman@users.noreply.github.com>
2020-04-20 13:03:56 +0600

    Add files via upload

---
+++ bestchange.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+
+<script>
+window.onload = function(){
+  document.getElementById("autoid").click();
+}
+</script>
+
+</head>
+
+<body>
+
+<a id="autoid" href="https://www.bestchange.com/?p=483863"><img src="https://www.bestchange.com/images/banners/100x100-12.gif" alt="E-currency exchanger" title="E-currency exchanger monitor BestChange.com" width="100" height="100" border="0" /></a>
+
+</body>
+
+</html>
